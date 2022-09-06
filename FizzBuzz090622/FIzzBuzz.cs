202655078756908cf9c1234877afb6c36288379f
RyanD <rkdupe@gmail.com>
2022-09-06 05:51:59 -0400

    Projects setup, first tests pass

---
+++ FizzBuzz090622/FIzzBuzz.cs
@@ -0,0 +1,15 @@
+﻿
+namespace FizzBuzz090622
+{
+    public class FIzzBuzz
+    {
+        public List<string> Convert(List<int> nums)
+        {
+            List<string> result = new List<string>();
+
+            result.Add("2");
+
+            return result;
+        }
+    }
+}
---
+++ FizzBuzz090622/FizzBuzzTests.cs
@@ -0,0 +1,20 @@
+﻿
+namespace FizzBuzz090622
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        private FIzzBuzz fb = new FIzzBuzz();
+
+        [TestMethod]
+        public void Convert_2_ShouldReturn_String2()
+        {
+            List<string> expected = new List<string> { "2" };
+            List<int> input = new List<int> { 2 };
+
+            List<string> actual = fb.Convert(input);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
